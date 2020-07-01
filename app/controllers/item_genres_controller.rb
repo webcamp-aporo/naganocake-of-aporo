c074b4243b405586efc967bdf93e3b32b5aeb5ba
vagrant <kf0830s@gmail.com>
2020-07-01 22:33:52 +0000

    Add: controller

---
+++ app/controllers/item_genres_controller.rb
@@ -0,0 +1,7 @@
+class ItemGenresController < ApplicationController
+  def index
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'item_genres/index'
+  get 'item_genres/edit'
   devise_for :customers
   devise_for :admins
+  namespace :admin do
+    resources :item_genres, only: [:index, :create, :edit, :update]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
