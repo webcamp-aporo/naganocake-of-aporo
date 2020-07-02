301df8ba4f9330a23bcdc899673873e169e770a1
vagrant <nori.work.vtr250@gmail.com>
2020-07-02 05:45:48 +0000

    add edit to admins/customer/controller,views

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -8,7 +8,18 @@ class Admins::CustomersController < ApplicationController
   end
 
   def edit
+  	@customer = Customer.find(params[:id])
   end
 
-  
+  def update
+  	customer = Customer.find(params[:id])
+  	customer.update(customer_params)
+  	redirect_to customer_path(customer) #リダイレクト先は要確認
+  end
+
+  	# ストロングパラメーターは未定
+  	# private
+  	# def customer_params
+  	# 	params.require(:customer).permit()
+  	# end
 end
