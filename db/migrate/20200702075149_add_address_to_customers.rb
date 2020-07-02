e49e0cab3a6812dd673696ef34370fbc51d84e35
vagrant <vagrant@localhost.localdomain>
2020-07-02 10:10:31 +0000

    add customers

---
+++ db/migrate/20200702075149_add_address_to_customers.rb
@@ -0,0 +1,12 @@
+class AddAddressToCustomers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :customers, :address, :string
+    add_column :customers, :last_name, :string
+    add_column :customers, :first_name, :string
+    add_column :customers, :last_name_kana, :string
+    add_column :customers, :first_name_kana, :string
+    add_column :customers, :postal_number, :string
+    add_column :customers, :phone_number, :string
+    add_column :customers, :is_delete, :boolean, default: false
+  end
+end
