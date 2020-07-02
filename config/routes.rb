a51ae120338559827b0078b8a2f2a0bdc2c0e4db
vagrant <nori.work.vtr250@gmail.com>
2020-07-02 04:51:34 +0000

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   devise_for :customers
   devise_for :admins
   namespace :admins do
+  	resources :customers, only: [:index, :show, :edit, :update]
     resources :item_genres, only: [:index, :create, :edit, :update]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
