8c3c484cbb31fe0bcec18e8c25d32b5d33c08a5c
vagrant <nori.work.vtr250@gmail.com>
2020-07-03 02:44:21 +0000

    add customer-sample to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+5.times do |n|
+	Customer.create!(
+		email: "test#{n + 1}@test.com}",
+		password: "tttttt#{n + 1}",
+		address: "東京都1ー1ー1",
+		last_name: "田中#{n + 1}",
+		first_name: "太郎#{n + 1}",
+		last_name_kana: "タナカ#{n + 1}",
+		first_name_kana: "タロウ#{n + 1}",
+		postal_number: "123456",
+		phone_number: "11122223333",
+		is_delete: false
+	)
+end
