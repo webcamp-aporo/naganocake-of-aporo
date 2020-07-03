d16b57d05ef1332ef911905abe3b07d0a49b1f5c
vagrant <kf0830s@gmail.com>
2020-07-03 08:40:12 +0000

    Add: items each page

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  belongs_to :item_genre
+  attachment :image
+
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :item_genre_id, presence: true
+  validates :price, presence: true
+  validates :is_active, presence: true
 end
--- app/models/item_genre.rb
+++ app/models/item_genre.rb
@@ -1,5 +1,7 @@
 class ItemGenre < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   #真偽値の存在をチェックしたい場合には以下を記述
   validates :is_delete, inclusion: { in: [true, false] }
+
+  has_many :items
 end
