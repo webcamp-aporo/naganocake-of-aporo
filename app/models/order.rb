336b478a5950088c6fff7b2e670671de4cb2f269
vagrant <nori.work.vtr250@gmail.com>
2020-07-06 13:28:07 +0000

    add enum to order_items.rb ordre.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ApplicationRecord
 	has_many :order_items, dependent: :destroy
+	enum :payment_method, { 銀行振込:0, クレジットカード:1 }
+	enum :order_status, { 入金待ち:0, 入金確認:1, 製作中:2, 発送準備中:3, 発送済み:4 }
 end
