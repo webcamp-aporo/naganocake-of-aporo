0e7c5014f895dc2e848d383bda7558001fb24b37
vagrant <kf0830s@gmail.com>
2020-07-08 09:06:45 +0000

    Fix: layout

--- db/schema.rb
+++ db/schema.rb
@@ -98,7 +98,6 @@ ActiveRecord::Schema.define(version: 2020_07_06_124514) do
     t.string "postal_number", null: false
     t.string "address", null: false
     t.string "name", null: false
-    t.integer "customer_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
