a9a2610d5b9dd7269afd418eb3c168e0d78fee4c
vagrant <vagrant@localhost.localdomain>
2020-07-09 09:55:36 +0000

    mod/order

--- app/controllers/customers/items_controller.rb
+++ app/controllers/customers/items_controller.rb
@@ -1,19 +1,19 @@
 class Customers::ItemsController < ApplicationController
 	def index
-		@items = Item.all
-		@genres = ItemGenre.all
+		@items = Item.where(is_active: true)
+		@genres = ItemGenre.where(is_delete: false)
 	end
 
 	def show
 		@item = Item.find(params[:id])
-	    @genres = ItemGenre.all
+	    @genres = ItemGenre.where(is_delete: false)
 		@cart_item = current_customer.cart_items.build
 	end
 
 	def genres										#最終的にはAjax化したい…
 		@items = Item.where(item_genre_id: params[:genres_id])
 	    @genre = ItemGenre.find(params[:genres_id])
-		@genres = ItemGenre.all
+		@genres = ItemGenre.where(is_delete: false)
 	end
     
   private
