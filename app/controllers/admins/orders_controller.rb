a861959bd4c005ac80872f35a115ec665c53f7c0
vagrant <kf0830s@gmail.com>
2020-07-10 10:00:06 +0000

    Fix: conditional branch logged_in?

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -1,4 +1,6 @@
 class Admins::OrdersController < ApplicationController
+	before_action :authenticate_admin!
+	
   def index
   	@orders = Order.page(params[:page]).per(10).order('created_at DESC')
   end
