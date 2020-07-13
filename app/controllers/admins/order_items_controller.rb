82e15b72fbd0603d605b28c8471506ea4b098626
vagrant <kf0830s@gmail.com>
2020-07-13 13:18:38 +0000

    Fix: order_items contoroller

--- app/controllers/admins/order_items_controller.rb
+++ app/controllers/admins/order_items_controller.rb
@@ -3,7 +3,14 @@ class Admins::OrderItemsController < ApplicationController
 
 	def update_make
 		@order = OrderItem.find(params[:id])
-		@order.update(update_params)
+		@order_item = @order.order
+		if update_params[:make_status] == '製作中'
+			@order.update(update_params)
+			@order_item.update(order_status: "製作中")
+		elsif update_params[:make_status] == '製作完了'
+			@order.update(update_params)
+			@order_item.update(order_status: "発送準備中")
+		end
 		redirect_to admins_order_path(@order.order)
 	end
 
